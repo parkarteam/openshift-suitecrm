a673a3f242c7091f693275d983060de7cb368e48
Kiran Kumar <gmkumar2005@gmail.com>
2020-04-17 17:52:45 +0530

    csfr fix

--- suitecrmimage/config_override.php
+++ suitecrmimage/config_override.php
@@ -1,5 +1,5 @@
 <?php
 /***CONFIGURATOR***/
 $sugar_config['disable_persistent_connections'] = false;
-$sugar_config['http_referer']['list'][] = 'suitecrm-suitecrmdev.apps.nexgen.parkar.consulting';
-$sugar_config['http_referer']['actions'] =array( 'index', 'ListView', 'DetailView', 'EditView', 'oauth', 'authorize', 'Authenticate', 'Login', 'SupportPortal', 'ajaxui' ); 
+$sugar_config['http_referer']['list'][] = $sugar_config['host_name'];
+$sugar_config['http_referer']['actions'] =array( 'index', 'ListView', 'DetailView', 'EditView', 'oauth', 'authorize', 'Authenticate', 'Login', 'SupportPortal', 'ajaxui' );
